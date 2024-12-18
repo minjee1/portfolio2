bd2974e2fc41597e8843d73fed26d0eeb14dd9ee
minjee1 <min_vely__@naver.com>
2024-12-18 13:55:50 +0900

    js update

--- js/common.js
+++ js/common.js
@@ -1,3 +1,10 @@
-${function () {
-    
-}};
+$(function () {
+    // 실행문
+    $("record_:not(:first)").css("display", "none");
+    $("accordion accordion-toggle").click(function () {
+      if ($("+record_", this).css("display") === "none") {
+        $("record_").slideUp("slow");
+        $("+record_", this).slideDown("slow");
+      }
+    }); 
+  });
